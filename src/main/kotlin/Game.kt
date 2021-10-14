4b9aa3ed3e691abc07933924eb2e8207d4a9c0bd
lkimilhol <lkimilhol@gmail.com>
2021-10-14 22:11:12 +0900

    refactor(Game): 랜덤 숫자 생성 로직 구현

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,3 +1,14 @@
 class Game {
-    val numbers: List<Int> = listOf(1,2,3)
+
+    val numbers: Set<Int> = generateRand()
+
+    fun generateRand(): Set<Int> {
+        val numbers: MutableSet<Int> = mutableSetOf()
+        while (numbers.size < 3) {
+            val rand = (1..9).random()
+            numbers.add(rand)
+        }
+        return numbers
+    }
 }
+
