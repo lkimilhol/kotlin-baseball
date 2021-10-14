57f500500e8b7ccd82bf71de7c5cdde4ed934ff8
lkimilhol <lkimilhol@gmail.com>
2021-10-14 22:20:24 +0900

    refactor(Game): 매직 넘버 제거

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,14 +1,18 @@
 class Game {
+    companion object {
+        const val NUMBER_SIZE = 3
+        const val NUMBER_RANGE_MIN = 1
+        const val NUMBER_RANGE_MAX = 9
+    }
 
     val numbers: Set<Int> = generateRand()
 
-    fun generateRand(): Set<Int> {
+    private fun generateRand(): Set<Int> {
         val numbers: MutableSet<Int> = mutableSetOf()
-        while (numbers.size < 3) {
-            val rand = (1..9).random()
+        while (numbers.size < NUMBER_SIZE) {
+            val rand = (NUMBER_RANGE_MIN..NUMBER_RANGE_MAX).random()
             numbers.add(rand)
         }
         return numbers
     }
 }
-
