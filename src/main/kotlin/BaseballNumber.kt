e32953931b6c653e4feec9dbd328a67cf6cb8e65
lkimihol <lkimilhol@gmail.com>
2021-10-20 20:25:39 +0900

    refactor(BaseballNumber): data 클래스로 수정

--- src/main/kotlin/BaseballNumber.kt
+++ src/main/kotlin/BaseballNumber.kt
@@ -1,4 +1,4 @@
-class BaseballNumber (val number: Int) {
+data class BaseballNumber (val number: Int) {
     companion object {
         const val NUMBER_RANGE_MIN = 1
         const val NUMBER_RANGE_MAX = 9
@@ -6,20 +6,5 @@ class BaseballNumber (val number: Int) {
 
     init {
         if (number < NUMBER_RANGE_MIN || number > NUMBER_RANGE_MAX) throw IllegalArgumentException()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as BaseballNumber
-
-        if (number != other.number) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return number
     }
 }
